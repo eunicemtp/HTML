7729155a65fdd601aaa8780cd4d28f8f5cee0e47
AndreaVasquez003 <151018092+AndreaVasquez003@users.noreply.github.com>
2025-04-25 17:36:20 +0200

    animation

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -48,6 +48,103 @@ button.addEventListener('mouseleave', () => {
   }, 400);
 });
 
+gsap.to(".carre3", {x:"-100%", duration: 1, ease: "power3.out"});
+gsap.to(".carre2", {x:"-100%", duration: 1, delay:0.3, ease: "power3.out"});
+gsap.to(".carre1", {x:"-100%",duration: 1, delay:0.5, ease: "power3.out"});
+
+
+// Mouse particles
+var canvas = $('canvas').get(0);
+var ctx = canvas.getContext('2d');
+var particles = [];
+var mousePos = {
+	x: window.innerWidth / 2,
+	y: window.innerHeight / 2
+};
+ 
+console.log(mousePos);
+ 
+var multiply = 50;
+ 
+function draw() {
+	ctx.clearRect(0,0,canvas.width, canvas.height);
+	if (particles.length < multiply) {
+		for(let i = 0; i < multiply; i++) {
+			let p = new Particle();
+			particles.push(p);
+		}
+	}
+ 
+	for(let i = particles.length - 1; i >= 0; i--) {
+		particles[i].update();
+		particles[i].draw();
+	}
+	requestAnimationFrame(draw);
+}
+ 
+ 
+class Particle {
+	constructor(){
+		this.reset();
+	}
+	reset() {
+		this.radius = 10;
+		this.x = mousePos.x;
+		this.y = mousePos.y;
+		this.explosionRadius = 2;
+		this.angle = random(0 ,Math.PI * 2);
+		this.color = 250;
+		this.velocity = {
+			x: Math.sin(this.angle) * this.explosionRadius,
+			y: Math.cos(this.angle) * this.explosionRadius,
+		};
+		this.alpha = Math.random();
+	}
+	/*draw() {
+		ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 1, 2 * Math.PI, false);
+    ctx.fillStyle = `rgba(66, 245, 203, ${this.alpha})`;
+    ctx.fill();
+	}*/
+	draw() {
+    ctx.fillStyle = `rgba(173, 218, 189, ${this.alpha})`;
+    // Dessine un carré avec un côté de taille égale à 2 * this.radius
+    ctx.fillRect(this.x - this.radius, this.y - this.radius, 2 * this.radius, 2 * this.radius);
+}
+	update() {
+		this.x += this.velocity.x;
+		this.y += this.velocity.y;
+		this.alpha -= 0.01;
+		this.color = Math.abs(this.color - 5);
+		if (this.alpha < 0) {
+			this.reset();
+		}
+	}
+}
+
+ 
+function follow(e) {
+	mousePos.x = e.pageX;
+	mousePos.y = e.pageY;
+}
+ 
+ 
+function random(min,max) {
+	return Math.random()*(max-min+1)+min;
+}
+function resize() {
+	canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+}
+$(function(){
+	$(window).on('resize', resize);
+	$(canvas).on('mousemove', follow);
+	resize();
+	draw();
+});
+ 
+
+ 
 
 
 
