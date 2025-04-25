b18607a59fcf792a0b9fe5ff3ea7c99907dfba15
Eunice Mutope <120035932+eunicemtp@users.noreply.github.com>
2025-04-25 10:02:34 +0200

    clean code

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
-console.info('Hello world');
+'use strict'
+
+import { gsap } from "gsap";
+    
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { Draggable } from "gsap/Draggable";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+
+
+gsap.registerPlugin(ScrollTrigger,ScrollToPlugin,Draggable,MotionPathPlugin);
